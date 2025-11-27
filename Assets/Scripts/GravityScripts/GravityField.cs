322339fb37001bab26ead126b6424009423b5f89
STUDENT\2311052 <kaoru.ace07@gmail.com>
2025-11-27 16:46:56 +0900

    生成後の重力場が自動で削除されるようになったよ　 次回 生成個数制限バグの修正　

--- Assets/Scripts/GravityScripts/GravityField.cs
+++ Assets/Scripts/GravityScripts/GravityField.cs
@@ -5,18 +5,29 @@ public class GravityField : MonoBehaviour
     public float width;
     public float height;
 
+    // 自動削除までの残り時間（秒）
+    private float lifeTime;
+
     public float area => width * height;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // 面積/２秒に四捨五入した時間だけ生存
+        lifeTime = Mathf.Round(area / 2f);
+
+        //ログ
+        Debug.Log($"重力場生成: 面積={area:F1}, 生存時間={lifeTime}秒");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lifeTime -= Time.deltaTime;
+        if(lifeTime <= 0f)
+        {
+            // 削除時のログ出力
+            Debug.Log($"重力場自動削除: 面積={area:F1}, 生存時間終了");
+            Destroy(gameObject);
+        }
     }
-
-    
 }
